0c8b32307a84b9a79c50c23d600d31e61add79ad
Vladimir Novopashin <novopashinwm@mail.ru>
2021-06-05 22:16:41 +0300

    Исправил ошибки: 1) Вынес в extentions загрузку картинок. 2) Убрал ArtistCardContainer.kt и работу с 2 RecyclerView. 3) Заменил работу с like на контрол CheckBox 4) Изменил табличку на RelativeLayout.

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/ArtistItem.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/ArtistItem.kt
@@ -1,23 +1,21 @@
 package ru.androidschool.intensiv.ui.movie_details
 
-import com.squareup.picasso.Picasso
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.item_actor.*
 import kotlinx.android.synthetic.main.item_with_text.image_preview
 import ru.androidschool.intensiv.R
 import ru.androidschool.intensiv.data.Artist
+import ru.androidschool.intensiv.extensions.loadImage
 
-class ArtistItem (
+class ArtistItem(
     private val content: Artist
 ) : Item() {
 
     override fun getLayout() = R.layout.item_actor
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.FIO.text = content.fullname
-        Picasso.get()
-            .load("https://image.tmdb.org/t/p/w500${content.avatar}")
-            .into(viewHolder.image_preview)
+        viewHolder.fullname.text = content.fullname
+        viewHolder.image_preview.loadImage(content.avatar)
     }
 }
