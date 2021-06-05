9c090c9cc6af75e9239fbd5e43440e970e42b0d1
Vladimir Novopashin <novopashinwm@mail.ru>
2021-06-05 10:43:15 +0300

    Добавил подробный просмотр информации о фильме: 1) Пришлось добавить полей в класс Movie.kt, чтобы соответствовало проекту. 2) Понимаю, что сделал аляписто в movie_details_fragment.xml с LinearLayout, мне нужна более подрбная информация как более аккуратно выводить данные типа таблица. 3) Удалось скруглить кнопку для просмотра фильма, но не удалось скруглить фотки артистов. Наверняка какое-то свойство есть. 4) Как в MockDetailsRepository.kt вызвать getString, чтобы получить доступ к ресурсам - раньше я получал через Resources.getString - сейчас этот метод depricated. 5) Для просмотра артистов пришлось создать свои item в классах и верстке, а также контейнер для хранения этих артистов.

--- app/src/main/java/ru/androidschool/intensiv/data/Movie.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/Movie.kt
@@ -2,7 +2,13 @@ package ru.androidschool.intensiv.data
 
 class Movie(
     var title: String? = "",
-    var voteAverage: Double = 0.0
+    var voteAverage: Double = 0.0,
+    var overview: String? = "",
+    var artists : List<Artist> = emptyList(),
+    var studio : String? = "",
+    var genre: String? = "",
+    var year: String? = "",
+    var poster: String? = ""
 ) {
     val rating: Float
         get() = voteAverage.div(2).toFloat()
